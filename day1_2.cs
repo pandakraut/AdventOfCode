69cf9b1b02fd5bba62859a16b111678beda7a56b
pandakraut <75352338+pandakraut@users.noreply.github.com>
2020-12-02 11:06:39 -0500

    Add files via upload

---
+++ day1_2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020
+{
+    class day1_1
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set = new HashSet<int>();
+            bool done = false;
+
+            foreach (string line in File.ReadLines("../../../InputDay1_1.txt"))
+            {
+                int current = Int32.Parse(line);
+
+                if (set.Count > 2)
+                {
+                    foreach (int prior in set)
+                    {
+                        int compare = 2020 - current - prior;
+                        if (compare > 0 && set.Contains(compare))
+                        {
+                            Console.WriteLine("output: " + current * prior * compare);
+                            done = true;
+                            break;
+                        }
+                    }
+                    
+                }      
+                if (!done)
+                {
+                    set.Add(current);
+                }
+                else
+                {
+                    break;
+                }
+            }                                               
+        }        
+    }
+}
