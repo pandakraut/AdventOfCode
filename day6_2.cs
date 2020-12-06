bc3388418944eae02a0824a0870da2c8b28066c0
pandakraut <75352338+pandakraut@users.noreply.github.com>
2020-12-06 00:51:01 -0500

    Add files via upload

---
+++ day6_2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020
+{
+    class day6_2
+    {
+        public static Dictionary<char, int> questions = new Dictionary<char, int>(26);
+
+        static void Main(string[] args)
+        {
+            int sumCounts = 0;
+            int groupSize = 0;
+
+            foreach (string line in File.ReadLines("../../../InputDay6.txt"))
+            {
+                if (line.Length == 0)
+                {
+                    sumCounts += countAnswers(groupSize);
+                    groupSize = 0;
+                    questions.Clear();
+                }
+                else
+                {
+                    groupSize++;
+                    foreach (char c in line)
+                    {
+                        if (!questions.ContainsKey(c))
+                        {
+                            questions.Add(c,1);
+                        }
+                        else
+                        {
+                            questions[c]++;
+                        }
+                    }
+                }
+            }
+
+            //Account for no return at end of file
+            sumCounts += countAnswers(groupSize);
+
+            Console.WriteLine("output: " + sumCounts);
+        }
+
+        public static int countAnswers(int groupSize)
+        {
+            int answers = 0;
+            foreach (KeyValuePair<char, int> pair in questions)
+            {
+                if (pair.Value == groupSize)
+                {
+                    answers++;
+                }
+            }
+            return answers;
+        }
+    }
+}
